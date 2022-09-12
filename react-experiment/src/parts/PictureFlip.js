f3e1621c570aab891b328bb3efc217a0a381b90b
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-12 13:42:32 -0400

    Update PictureFlip.js

--- react-experiment/src/parts/PictureFlip.js
+++ react-experiment/src/parts/PictureFlip.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './PictureFlip.css';
 
 const unsplashUrl = "https://api.unsplash.com/";
+const accessKey = `${process.env.REACT_APP_UNSPLASH_access_key}`;
 
 function PictureFlip() {
   return (
