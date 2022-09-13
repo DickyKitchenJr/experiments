829e548c15e6351dd3792f7c02b02487de5a5b25
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-13 11:07:58 -0400

    Update PictureFlip.js

--- react-experiment/src/parts/PictureFlip.js
+++ react-experiment/src/parts/PictureFlip.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './PictureFlip.css';
 
 const unsplashUrl = "https://api.unsplash.com/";
 const accessKey = `${process.env.REACT_APP_UNSPLASH_access_key}`;
 const urlRequest = unsplashUrl + "photos?client_id=" + accessKey;
 
-console.log(unsplashUrl+'photos?client_id='+accessKey);
-console.log(urlRequest);
 
 function PictureFlip() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async() => {
+      const response = await fetch(urlRequest)
+      const data = await response.json()
+      setImages(data)
+    }
+
+    fetchImages()
+  }, [])
+
   return (
-    <div>PictureFlip</div>
+    <>
+      <h1>{images.length}</h1>
+    </>
   )
 }
 
