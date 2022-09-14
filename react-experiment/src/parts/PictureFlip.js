f5835224122d48a421e4ce40b6be91904295b886
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-14 13:43:09 -0400

    Update PictureFlip.js

--- react-experiment/src/parts/PictureFlip.js
+++ react-experiment/src/parts/PictureFlip.js
@@ -7,9 +7,12 @@ const accessKey = `${process.env.REACT_APP_UNSPLASH_access_key}`;
 const urlRequest = unsplashUrl + "photos?client_id=" + accessKey;
 
 
+// GOAL = to retrieve images from unsplash api and display one image at a time that the user can push a button to advance to the next image
+
 function PictureFlip() {
   const [images, setImages] = useState([]);
 
+  // creates a useEffect that, when called, will retrieve information from the unsplash api
   useEffect(() => {
     const fetchImages = async() => {
       const response = await fetch(urlRequest)
@@ -17,6 +20,7 @@ function PictureFlip() {
       setImages(data)
     }
 
+    // calls the unsplash api
     fetchImages()
   }, [])
 
