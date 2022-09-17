11066e0ba767cc0cf7f227f987a3aa339c7920f8
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-17 14:39:31 -0400

    Update Greeting.js

--- react-experiment/src/parts/Greeting.js
+++ react-experiment/src/parts/Greeting.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { useState } from 'react';
-import './Greeting.css';
+import React from "react";
+import { useState } from "react";
+import "./Greeting.css";
 
 function Greeting() {
-  const [userName, setUserName] = useState('Default User')
-    
+  const [userName, setUserName] = useState("Default User");
+
   return (
     <>
-        <form>
-            <label for="userName">What's your name?</label>
-            <input type='text' defaultValue={userName} id='userName' name='userName' onChange={(e) => setUserName(e.target.value)} />
-        </form>
+      <form>
+        <label for="userName">What's your name?</label>
+        <input
+          type="text"
+          defaultValue={userName}
+          id="userName"
+          name="userName"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </form>
       <h1>Hi {userName}. Enjoy these random pictures.</h1>
     </>
   );
 }
 
-export default Greeting
+export default Greeting;
