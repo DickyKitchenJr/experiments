246b6e6c2ffa03bc45ad69e1d05df95a691274ec
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-27 13:42:58 -0400

    Update PictureFlip.js

--- react-experiment/src/parts/PictureFlip.js
+++ react-experiment/src/parts/PictureFlip.js
@@ -6,7 +6,18 @@ import Greeting from "./Greeting";
 
 const unsplashUrl = "https://api.unsplash.com/";
 const accessKey = `${process.env.REACT_APP_UNSPLASH_access_key}`;
-const urlRequest = unsplashUrl + "photos?client_id=" + accessKey;
+
+const urlRequestRandom = unsplashUrl + "photos?client_id=" + accessKey;
+
+const urlRequestCats =
+  unsplashUrl +
+  "search/photos?query=cats&page=1&per_page=10&client_id=" +
+  accessKey;
+
+const urlRequestDogs =
+  unsplashUrl +
+  "search/photos?query=dogs&page=1&per_page=10&client_id=" +
+  accessKey;
 
 // console.log(urlRequest);
 
@@ -28,7 +39,7 @@ function PictureFlip() {
   // creates a useEffect that, when called, will retrieve information from the unsplash api
   useEffect(() => {
     const fetchImages = async () => {
-      const response = await fetch(urlRequest);
+      const response = await fetch(urlRequestRandom);
       const data = await response.json();
       setImages(data);
       // console.log(data);
