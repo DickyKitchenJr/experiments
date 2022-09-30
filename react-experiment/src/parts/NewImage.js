b9eb90a665c8819a7e19280874dbfc01266b1c8d
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-09-30 13:10:36 -0400

    Update NewImage.js

--- react-experiment/src/parts/NewImage.js
+++ react-experiment/src/parts/NewImage.js
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 import "./Images.css";
 
 function NewImage() {
-// PURPOSE: useState for API data to be set
-const [pictures, setPictures] = useState(null)
+  // PURPOSE: useState for API data to be set
+  const [pictures, setPictures] = useState(null);
 
-// PURPOSE: API call address
+  // PURPOSE: API call address
   const unsplashUrl = "https://api.unsplash.com/";
   const accessKey = `${process.env.REACT_APP_UNSPLASH_access_key}`;
   const urlRequestRandom = unsplashUrl + "photos?client_id=" + accessKey;
 
-// PURPOSE: useEffect for calling API and setting data in setPictures
+  // PURPOSE: useEffect for calling API and setting data in setPictures
   useEffect(() => {
     fetch(urlRequestRandom)
       .then((res) => res.json())
@@ -26,29 +26,31 @@ const [pictures, setPictures] = useState(null)
         });
         setPictures(photos);
         // FIXME: pictures not being updated by setPictures(photos), why?
-        if(pictures){console.log(pictures);}
+        if (pictures) {
+          console.log(pictures);
+        }
       });
-  }, [])
+  }, []);
 
-//   PURPOSE: iterate through pictures and return img and text for each
-// FIXME: pictures remains null, not updating through useEffect, why?
-  const ShowPhotos = () =>{
-    for(const items in pictures){
-       return <><img src={items.img} alt="random pic" loading="lazy" />
-       <h3>
-        Picture by:
-        <a href={items.html} target="_blank" rel="noreferrer">
-          {items.user}
-        </a>
-      </h3></>
+  //   PURPOSE: iterate through pictures and return img and text for each
+  // FIXME: pictures remains null, not updating through useEffect, why?
+  const ShowPhotos = () => {
+    for (const items in pictures) {
+      return (
+        <>
+          <img src={items.img} alt="random pic" loading="lazy" />
+          <h3>
+            Picture by:
+            <a href={items.html} target="_blank" rel="noreferrer">
+              {items.user}
+            </a>
+          </h3>
+        </>
+      );
     }
-  }
-  
-  return (
-    <>
-    {pictures && <ShowPhotos />}
-    </>
-  );
+  };
+
+  return <>{pictures && <ShowPhotos />}</>;
 }
 
 export default NewImage;
