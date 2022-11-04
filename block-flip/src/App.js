dfc19df16eceb6e320ef40b1c4b5f831b7a8b756
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-11-04 16:02:29 -0400

    code formatted

--- block-flip/src/App.js
+++ block-flip/src/App.js
@@ -1,20 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className='block-group'>
-      <div className='block topLeft'><p>X</p></div>
-      <div className='block topCenter'><p>X</p></div>
-      <div className='block topRight'><p>X</p></div>
-      <div className='block middleLeft'><p>X</p></div>
-      <div className='block middleCenter'><p>X</p></div>
-      <div className='block middleRight'><p>X</p></div>
-      <div className='block bottomLeft'><p>X</p></div>
-      <div className='block bottomCenter'><p>X</p></div>
-      <div className='block bottomRight'><p>X</p></div>
-    </div>
+      <div className="block-group">
+        <div className="block topLeft">
+          <p>X</p>
+        </div>
+        <div className="block topCenter">
+          <p>X</p>
+        </div>
+        <div className="block topRight">
+          <p>X</p>
+        </div>
+        <div className="block middleLeft">
+          <p>X</p>
+        </div>
+        <div className="block middleCenter">
+          <p>X</p>
+        </div>
+        <div className="block middleRight">
+          <p>X</p>
+        </div>
+        <div className="block bottomLeft">
+          <p>X</p>
+        </div>
+        <div className="block bottomCenter">
+          <p>X</p>
+        </div>
+        <div className="block bottomRight">
+          <p>X</p>
+        </div>
+      </div>
     </>
   );
 }
