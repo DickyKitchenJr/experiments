5f14513fa76d353bde55caac1dd82e8cf4a0b5e5
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-11-07 12:11:54 -0500

    completed animation and click for 2 blocks

--- block-flip/src/App.js
+++ block-flip/src/App.js
@@ -1,35 +1,72 @@
-import logo from "./logo.svg";
+import React, { useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [press, setPress] = useState('');
+
+  const handleClickCenter = () => {
+    if(press === ''){
+      setPress('center');
+    }
+  }
+
+  const handleClickTopLeft = () => {
+    if(press === ''){
+      setPress("topLeft");
+    }
+  };
+
+  const resetPress = () =>{
+    setTimeout(() => {
+      setPress('')
+    }, 1000);
+  }
+
+  const resetPressforCenter = () => {
+    setTimeout(() => {
+      setPress("");
+    }, 600);
+  };
+
+  // GOAL: to create a grid of blocks that trigger an animation when each block is clicked
   return (
     <>
       <div className="block-group">
-        <div className="block topLeft">
+        <div
+          className="block topLeft"
+          onClick={() => handleClickTopLeft()}
+          press={press}
+          onAnimationEnd={() => resetPress()}
+        >
           <p>X</p>
         </div>
-        <div className="block topCenter">
+        <div className="block topCenter" press={press}>
           <p>X</p>
         </div>
-        <div className="block topRight">
+        <div className="block topRight" press={press}>
           <p>X</p>
         </div>
-        <div className="block middleLeft">
+        <div className="block middleLeft" press={press}>
           <p>X</p>
         </div>
-        <div className="block middleCenter">
+        <div
+          className="block middleCenter"
+          onClick={() => handleClickCenter()}
+          onAnimationEnd={() => resetPressforCenter()}
+          press={press}
+        >
           <p>X</p>
         </div>
-        <div className="block middleRight">
+        <div className="block middleRight" press={press}>
           <p>X</p>
         </div>
-        <div className="block bottomLeft">
+        <div className="block bottomLeft" press={press}>
           <p>X</p>
         </div>
-        <div className="block bottomCenter">
+        <div className="block bottomCenter" press={press}>
           <p>X</p>
         </div>
-        <div className="block bottomRight">
+        <div className="block bottomRight" press={press}>
           <p>X</p>
         </div>
       </div>
