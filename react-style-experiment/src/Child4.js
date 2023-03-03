dd6546e091e1f2458aa15ec488104fe1d4079c30
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-03 11:02:30 -0500

    Create Child4.js

---
+++ react-style-experiment/src/Child4.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Child4(props) {
+  return (
+    <div>
+      <p className={`${props.styling}app`}>I am child 4</p>
+    </div>
+  )
+}
+
+export default Child4
