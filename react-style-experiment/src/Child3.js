5fa6c995528b3aa9b86706873bf7fab03a40efa4
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-03 11:02:28 -0500

    Create Child3.js

---
+++ react-style-experiment/src/Child3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Child3(props) {
+  return (
+    <div>
+      <p className={`${props.styling}app`}>I am child 3</p>
+    </div>
+  );
+}
+
+export default Child3
