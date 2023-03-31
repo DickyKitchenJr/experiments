34a5fe2c475098ebc5911ac2d7d686ad764c00e0
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-03-31 13:30:35 -0400

    continuing to follow nextjs.org tutorial

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,5 +1,7 @@
 // when you first create _app.js you have to restart the 
 // development server in order for it to take effect
+// _app.js is the ONLY place that you can import global styles
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
