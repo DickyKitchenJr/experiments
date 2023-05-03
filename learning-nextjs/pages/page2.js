d9b9fde33adb50b4a1fb51145fc22e08eef01175
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-03 10:12:50 -0400

    Create page2.js

---
+++ learning-nextjs/pages/page2.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Page2() {
+    return(
+        <>
+        <p>This is page 2</p>
+        <Link href='/'>Back to home</Link>
+        </>
+    )
+}
