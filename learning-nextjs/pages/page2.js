01ffeedefe303bde3acfe6121f716d77f6e41025
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-04 16:39:25 -0400

    Update page2.js

--- learning-nextjs/pages/page2.js
+++ learning-nextjs/pages/page2.js
@@ -21,7 +21,19 @@ export default function Page2() {
         For example, links to this page (from within the site) would need{" "}
         <strong>href='/page2'</strong> assigned within the Link element.
       </p>
+      <p>
+        It is important to note that, while the file name of your pages can be
+        lowercase, the function name within the file that you use to return the
+        page must be uppercase. For example, while the main/home page file name
+        is <strong>index.js</strong>, the actual function name is{" "}
+        <strong>Home</strong>. Similarly, while the file name for this page is <strong>page2.js</strong>, the function name is <strong>Page2</strong>.</p> 
+        <p>This is a Reactjs rule and not only applicable to
+        Nextjs, but it felt worth mentioning as it will save you headaches in
+        the future to remember to capitalize your function name.
+      </p>
       <Link href="/">Back to home</Link>
+      <br />
+      <Link href="/page3">Page 3</Link>
     </>
   );
 }
