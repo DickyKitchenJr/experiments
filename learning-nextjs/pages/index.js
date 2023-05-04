446312d76fb9d7dab5e777b05851884474c42d16
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-04 16:39:19 -0400

    Update index.js

--- learning-nextjs/pages/index.js
+++ learning-nextjs/pages/index.js
@@ -3,19 +3,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1>Main Parent (Home Page [index.js])</h1>
-      <Link href="/page2">Page 2</Link>
+      <h2>Main Parent (Home Page [index.js])</h2>
+
       <p>
         This is the main page, or home page of the site. It is stored in the{" "}
         <strong>index.js</strong> file within the <strong>pages</strong> folder,
-        and is the starting point of this nextjs build.
+        and is the starting point of this Nextjs app.
       </p>
       <p>
         You can think of it similar to the index.html file used when building an
         HTML page.
       </p>
       <p>
-        With nextjs, all of your pages will be stored in a{" "}
+        With Nextjs, all of your pages will be stored in a{" "}
         <strong>pages</strong> folder with the main/home page named{" "}
         <strong>index.js</strong>.
       </p>
@@ -24,6 +24,8 @@ export default function Home() {
         however other pages can be named whatever you'd like provided they are
         in the <strong>pages</strong> folder.
       </p>
+
+      <Link href="/page2">Page 2</Link>
     </>
   );
 }
