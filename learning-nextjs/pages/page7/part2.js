de57e09aa34959ee86bdbc5fdbdc5fec8e01f427
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-09 13:58:11 -0400

    Create part2.js

---
+++ learning-nextjs/pages/page7/part2.js
@@ -0,0 +1,9 @@
+export default function Page7Part2(){
+    return(
+        <>
+          <h2>Nesting Routes part 2</h2>
+
+          <p>As you can see, this makes it easier for the user to know where they are in the site as well as making it better for orgainizing the code.</p>
+        </>
+    )
+}
