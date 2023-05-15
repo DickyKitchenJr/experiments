35c319f4bf61ad210056de27caf1eded84750ab1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-05-15 14:16:18 -0400

    Create Header.jsx

---
+++ learning-vite/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/header.module.css';
+
+function Header() {
+  return (
+    <>
+      <h1 className={styles.h1}>I'm the REAL Header!</h1>
+    </>
+  )
+}
+
+export default Header
+
