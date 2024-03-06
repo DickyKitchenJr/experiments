151ac0d77dcba64c3126f41f408d0a1c7ca6a091
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-06 13:47:49 -0500

    Create App.jsx

---
+++ MillsEcwid/src/App.jsx
@@ -0,0 +1,12 @@
+import { ProductBrowser } from "@ecwid/nextjs-ecwid-plugin";
+
+function App() {
+
+  return (
+    <>
+      <ProductBrowser storeId="99936715" />
+    </>
+  );
+}
+
+export default App
