814e2aeb3aab666569bd117bce0372305abffab7
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-06 18:29:34 -0500

    updated header per client request

--- MillsEcwid/src/App.jsx
+++ MillsEcwid/src/App.jsx
@@ -1,9 +1,22 @@
 import { ProductBrowser } from "@ecwid/nextjs-ecwid-plugin";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+
+const house = <FontAwesomeIcon icon={faHouse} />
 
 function App() {
 
   return (
     <>
+      <h1 className="hero">
+        <a href="https://mayhemmills.com/" className="house">
+          {house}
+        </a>
+        <div className="logo">
+          MAYHEM<span>MILLS</span>.COM
+        </div>
+      </h1>
       <ProductBrowser storeId="99936715" />
     </>
   );
